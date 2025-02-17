0583cabf8d18ee663b6ac13d195d3eb2ec4b5b88
Jon Gainza <124215384+jongainza@users.noreply.github.com>
2025-02-17 14:03:17 -0800

    html and css finished

---
+++ data.js
@@ -0,0 +1,22 @@
+const people = [
+  {
+    img: "./images/gustavo-doe.jpg",
+    name: "gustavo doe",
+    job: "product manager",
+    text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem quoeius recusandae officia voluptas sint deserunt dicta nihil nam omnis? `,
+  },
+  {
+    img: "./images/maria-doe.jpg",
+    name: "maria doe",
+    job: "developer",
+    text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem quoeius recusandae officia voluptas sint deserunt dicta nihil nam omnis?`,
+  },
+  {
+    img: "./images/paco-doe.jpg",
+    name: "paco doe",
+    job: "designer",
+    text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem quoeius recusandae officia voluptas sint deserunt dicta nihil nam omnis?`,
+  },
+];
+
+export default people;
